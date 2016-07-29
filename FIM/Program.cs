cc3958aa0045789865c35bcef08b77af31b0b760
ibrahem-atef <ibrahematef@gmail.com>
2016-07-29 15:35:47 +0200

    modified the FIM starting class "program"
    now it displays useful info and asks for entering the names of the
    initialization file and the input data file.

--- FIM/Program.cs
+++ FIM/Program.cs
@@ -17,7 +17,27 @@ namespace FIM
         {
             //SimulationData data = Initialization.Model.initiaize();
 
-            SimulationData data = Parser.Eclipse.ReadFile("Odeh.Data");
+            Console.WriteLine(".................FIM Black-Oil Simulator..................");
+            Console.WriteLine("Make Sure the files are located within the same directory as the simulator .exe");
+            Console.WriteLine("Spell the file names correctly with their respective extensions, .INIT and .DATA.");
+            Console.WriteLine("Names are case insensitive.");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the initialization file name : ");
+            string initialization_file_name = Console.ReadLine();
+
+            Console.WriteLine("Enter the input data file name : ");
+            string data_file_name = Console.ReadLine();
+
+            Console.WriteLine();
+
+            SimulationData data = Parser.Eclipse.ReadInputData(data_file_name, initialization_file_name);
+
+
+
+            WriteInfo(data);
+
+
 
             if (data.solutionProcedure == Global.SolutionProcedure.FullyImplicit)
             {
@@ -28,6 +48,47 @@ namespace FIM
                 IMPES_Solver.RunSimulation(data);
             }
 
+            Console.ReadKey();
+        }
+
+        private static void WriteInfo(SimulationData data)
+        {
+            Console.WriteLine();
+
+            Console.Write("Title         : ");
+            Console.Write(data.title);
+            Console.WriteLine();
+
+            Console.Write("Solver        : ");
+            if (data.solutionProcedure == Global.SolutionProcedure.FullyImplicit)
+            {
+                Console.Write("Fully Implicit");
+            }
+            else
+            {
+                Console.Write("IMPES");
+            }
+            Console.WriteLine();
+
+            Console.Write("Ending Time   : ");
+            Console.Write(data.endingTime + " ");
+            Console.Write("days");
+            Console.WriteLine();
+
+            Console.Write("Grid Size     : ");
+            Console.Write(data.grid.Length + " ");
+            Console.Write("blocks");
+            Console.WriteLine();
+
+            Console.Write("Wells Present : ");
+            Console.Write(data.wells.Length + " ");
+            Console.Write("wells");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to begin the simulation ...");
             Console.ReadKey();
         }
     }
