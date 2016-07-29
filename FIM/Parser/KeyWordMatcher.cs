68437b35fc6ae1578c8c31a7493b45f0878be71a
ibrahem-atef <ibrahematef@gmail.com>
2016-07-29 11:33:54 +0200

    new class
    helper class that include extensions to the String class.

---
+++ FIM/Parser/KeyWordMatcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FIM.Parser
+{
+    public static class KeyWordMatcher
+    {
+        /// <summary>
+        /// Determines whether the line contains any of the specified key words.
+        /// </summary>
+        /// <param name="line">The line.</param>
+        /// <param name="KeyWords">The key words.</param>
+        /// <returns></returns>
+        public static bool ContainsAnyOf(this string line, string[] KeyWords)
+        {
+            for (int i = 0; i < KeyWords.Length; i++)
+            {
+                //if (line.Contains(KeyWords[i]))
+                //{
+                //    return true;
+                //}
+
+                if (Regex.Match(line, "\\b" + KeyWords[i] + "\\b").Success)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
