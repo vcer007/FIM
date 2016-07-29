d96b1fe797eb4ad23a61073029ad1ce2bae729b7
ibrahem-atef <ibrahematef@gmail.com>
2016-07-29 11:26:04 +0200

    added a new argument "multiplier"
    allows transmissibility calculation to take a multiplier as an input

--- FIM/Core/Transmissibility.cs
+++ FIM/Core/Transmissibility.cs
@@ -17,15 +17,16 @@ namespace FIM.Core
         /// </remarks>
         /// <param name="block_1">block_1.</param>
         /// <param name="block_2">block_2.</param>
+        /// <param name="multiplier">a multiplier used for altering inter-layers transmissibilities.</param>
         /// <returns>the Geometrical transmissibility between two blocks.</returns>
         /// <seealso cref="BaseBlock"/>
-        public static double calculate(BaseBlock block_1, BaseBlock block_2)
+        public static double calculate(BaseBlock block_1, BaseBlock block_2, double multiplier = 1)
         {
             // As each block stores a list of side faces areas, we need to find the index of each block relative to the other.
             int index_1, index_2;
 
-            index_1 = Array.IndexOf(block_1.neighbourBlocksIndices, block_2.index);
-            index_2 = Array.IndexOf(block_2.neighbourBlocksIndices, block_1.index);
+            index_1 = Array.IndexOf(block_1.neighborBlocksIndices, block_2.index);
+            index_2 = Array.IndexOf(block_2.neighborBlocksIndices, block_1.index);
 
             double length_1, area_1, permeability_1;
             double length_2, area_2, permeability_2;
@@ -38,8 +39,8 @@ namespace FIM.Core
             area_2 = block_2.areaList[index_2];
             permeability_2 = block_2.permeability[index_2];
 
-            double G = 2 * Global.Bc / (length_1 / (area_1 * permeability_1) + length_2 / (area_2 * permeability_2));
-            return G;
+            double G = Global.Bc / (length_1 / (area_1 * permeability_1) + length_2 / (area_2 * permeability_2));
+            return G * multiplier;
         }
     }
 }
